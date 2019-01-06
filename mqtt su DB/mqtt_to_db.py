7a829947f1baad2321796c4df38f6b36e02004cd
cesco78 <cesco78@users.noreply.github.com>
2019-01-06 16:51:20 +0100

    Gestione umidità e batteria
    Aggiunta tutta la parte che fa il parse del payload, in modo da salvare sul DB anche l'umidtà e la tensione della batteria

--- mqtt su DB/mqtt_to_db.py
+++ mqtt su DB/mqtt_to_db.py
@@ -22,7 +22,7 @@
 # Fare il backup del DB (non dimenticarsi di farlo!!)
 # comando --> echo '.dump' | sqlite3 domotica.db > exportdb
 #
-# Versione del 26/12/2018
+# Versione del 06/01/2019
 
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
@@ -40,6 +40,9 @@ percorsoDB = "/percorso/domotica.db"
 
 
 # Sottoscrizione a tutti i topic dei vari sensori
+# non so perche', ma in un progetto questo funzionava, in un altro no.
+# se il sistema non sottoscrive i topic, batra togliere questa funzuione e mettere
+# la sottoscrizione subito dopo la connessione.
 def on_connect(mosq, obj, rc):
 	mqttc.subscribe([(MQTT_Topic1, 0), (MQTT_Topic2, 0)])
 
@@ -65,19 +68,42 @@ def on_message(mosq, obj, msg):
             luogo = 1
             tabella = "Corrente"
             
+        #questa gestione dell'arrivo della temperatura gestisce anche l'umido e la batteria
         if tabella == "Temperature":    
             try:
                 connessione = sqlite3.connect(percorsoDB)
                 
-                cursore = connessione.cursor()
-                # inserisco la lettura della temperatura
-                cursore.execute("INSERT INTO Temperature (Luogo, Temp) VALUES (" + luogo + ", " + msg.payload + ");")
-        
+                # faccio lo split della stringa che arriva dalla scheda, il formato e' questo:
+                # "00,00 # 00,00 # 0,00" dove
+                # - il primo numero e' la temperatura in gradi celsius
+                # - il secodo numero e' l'umidita' in percentuale
+                # - il terzo numero e' la tensione della batteria
+                #
+                # da prove sperimentali, sotto i 2,55V le letture diventano irreali e mostrano i dati di minimo e massimo
+                # del sensore. Quindi se la temperatura e' -45 o 130 e l'umidita' e' 100 la batteria e' troppo bassa
+		#
+		# qui per ora manca la gestione dei dati falsati dalla batteria scarica
                 
-                print 'Ho registrato questo: zona ' + msg.topic + ' - Temperatura ' + msg.payload + '*C'
+                # faccio lo split della stringa
+                t,h,v = msg.payload.split("#")
+                
+                # tolgo gli spazi daventi e dietro
+                t = t.strip()
+                h = h.strip()
+                v = v.strip()
+                
+                cursore = connessione.cursor()
+                # inserisco la lettura dei dati nella tabella
+                cursore.execute("INSERT INTO Temperature (Luogo, Temp, Umid, Vbatt) VALUES (" + luogo + ", " + t + ", " + h + ", " + v +");") 
+                
+                print 'Ho registrato questo: zona ' + msg.topic + ' - Temperatura ' + t + '*C - Umid ' + h + '% - Batteria ' + v + 'V' 
             
             except sqlite3.Error, e:
                 return "Error %s:" % e.args[0]
+            
+            except Exception as e:
+                print("qualcosa non va")
+                print(e)
             
             finally:
                 if connessione:
@@ -85,15 +111,14 @@ def on_message(mosq, obj, msg):
                         connessione.commit()
                         connessione.close()
                     except sqlite3.Error, e:
-                        return "Error %s:" % e.args[0]
-        
+                        return "Error %s:" % e.args[0]        
         
         if tabella == "Corrente":
             try:
                 connessione = sqlite3.connect(percorsoDB)
                 
                 cursore = connessione.cursor()
-                # inserisco la lettura della corrente
+                # inserisco la lettura della temperatura
                 cursore.execute("INSERT INTO Corrente (Luogo, Consumo) VALUES (" + str(luogo) + ", " + str(msg.payload) + ");")
         
                 
@@ -110,7 +135,8 @@ def on_message(mosq, obj, msg):
                     except sqlite3.Error, e:
                         return "Error %s:" % e.args[0]
 
-
+        
+        
 def on_subscribe(mosq, obj, mid, granted_qos):
     pass
 
@@ -121,7 +147,18 @@ mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 
 # Connessione al broker
-mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
+try:
+    mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
+    print("connesso")
+except:
+    print("Problema di connessione")
+
+# sottoscrizione Topic, se la funzione di sopra non va
+try: 
+    mqttc.subscribe([(MQTT_Topic1, 0), (MQTT_Topic2, 0)])
+    print ("sottoscritti i topic")
+except:
+    print("qualcosa non va") 
 
 # Non mi fermo mai...
 mqttc.loop_forever()
