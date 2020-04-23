ba4e67f98f9a406b2c6c4ab95e77f546d84a2bbd
cesco78 <cesco78@users.noreply.github.com>
2020-04-23 22:57:17 +0200

    Create eink-display.py

---
+++ display eink/eink-display.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#
+# Visualizzare le informazioni del progetto www.ladomoticafaidamie.it in un modo semplice e immediato
+# un progetto di Francesco Tucci (https://www.iltucci.com)
+# versione 1.0 del 22/04/2020 (durante la quarantena per il Coronavirus)
+#
+# materiale necessario:
+# . Raspberry Pi (una versione qualsiasi, basta che abbia il GPIO da 40 pin)
+# . Scheda microSD
+# . ALimentatore per il Raspberry
+# . display Inky-wHat di Pimoroni https://shop.pimoroni.com/products/inky-what?variant=21214020436051
+#
+# Per preparare la scheda e' necessario fare riferimento all'articolo esplicativo sul blog:
+# tutti i "print" sparsi nel codice sono essenzialmente per debug, e una volta finito il progetto possono essere tolti.
